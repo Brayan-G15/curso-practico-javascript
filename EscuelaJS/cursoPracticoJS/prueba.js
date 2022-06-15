c1620690d175076a1c10c19b155fe76da2cb24a7
Brayan-g15 <b.sleider15@gmail.com>
2022-06-15 10:45:33 -0500

    Añadi el index.html con links de los documentos

---
+++ EscuelaJS/cursoPracticoJS/prueba.js
@@ -0,0 +1,78 @@
+// const name = "Juan David";
+// const lastname = "Castro Gallego";
+// const completeName = name  + lastname;
+// const nickname = "juandc";
+
+
+// function presentacion(completeName, nickname){
+//     console.log("Mi nombre es " + completeName + ", pero prefiero ue me digan " + nickname +".")
+
+// }
+
+// const tipoDeSuscripcion = "Basic";
+
+// switch (tipoDeSuscripcion) {
+//    case "Free":
+//        console.log("Solo puedes tomar los cursos gratis");
+//        break;
+//    case "Basic":
+//        console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+//        break;
+//    case "Expert":
+//        console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+//        break;
+//    case "ExpertPlus":
+//        console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
+//        break;
+// }
+
+// if (tipoDeSuscripcion == "Basic"){
+//     console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+// }else if (tipoDeSuscripcion == "free"){
+//     console.log("Solo puedes tomar los cursos gratis");
+// }else if(tipoDeSuscripcion == "expert"){
+//     console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+// }else{
+//     console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
+// }
+
+// var planes = ["free", "basic", "expert", "expertplus"]
+//  if (planes != [0]){
+
+//  }
+
+for (let i = 0; i < 5; i++) {
+    console.log("El valor de i es: " + i);
+}
+
+while (i < 5){
+    console.log("El valor de i es " + i);
+    i++;
+}
+
+for (let i = 10; i >= 2; i--) {
+    console.log("El valor de i es: " + i);
+}
+
+while (i >= 2){
+    console.log("El valor de i es: " + i);
+    i--
+}
+
+var pregunta = prompt("Cuanto es 2+ 2: ")
+
+for (let i = 0; i = 4; i++ ){
+    if (pregunta != 4){
+        console.log("respondiste maal, intentalo nuevamente")
+    } else{
+        console.log("Felicitaciones!")
+    }
+}
+
+while (pregunta == 4 ){
+    if (pregunta != 4){
+        console.log("respondiste maal, intentalo nuevamente")
+    } else{
+        console.log("Felicitaciones!")
+    }
+}
